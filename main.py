4a911ab5fa9e30b90d5ffb0d2419d935c978c7fd
Shahzebkhan <G00288899@stu.raritanval.edu>
2025-07-08 22:30:08 -0400

    Final Homework 6 setup and README updated

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from app import App
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+env = os.getenv("ENVIRONMENT")
+print(f"Environment: {env}")
 
 if __name__ == "__main__":
     app = App()
